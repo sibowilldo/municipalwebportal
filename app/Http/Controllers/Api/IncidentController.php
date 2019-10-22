d117648b7c062bc58a35d62127609fe4a87c15e9
Sibongiseni Msomi <siboingiseni.msomi@outlook.com>
2019-10-22 07:39:47 +0200

    Comments

--- app/Http/Controllers/Api/IncidentController.php
+++ app/Http/Controllers/Api/IncidentController.php
@@ -108,7 +108,7 @@ class IncidentController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return IncidentResource
      */
     public function show($id)
     {
@@ -121,7 +121,7 @@ class IncidentController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return IncidentResource
      */
     public function edit($id)
     {
