0f5c0ca12a0242cbeb3d5dad6098de54d7b32ea8
Sibongiseni Msomi <siboingiseni.msomi@outlook.com>
2019-11-13 22:30:48 +0200

    Log Device Send

--- app/Listeners/FCMSendToDeviceListener.php
+++ app/Listeners/FCMSendToDeviceListener.php
@@ -6,6 +6,7 @@ use App\Events\IncidentUpdatedEvent;
 use App\FCMNotification;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Log;
 
 class FCMSendToDeviceListener
 {
@@ -21,11 +22,17 @@ class FCMSendToDeviceListener
         if(count($devices)){
             foreach ($devices as $device){
                 $fcm = new FCMNotification();
-                $fcm->sendToDevice(
-                            $device->token,
+                if ($device->token){
+                    $fcm->sendToDevice(
+                        $device->token,
                         'Incident Updated',
-                            $event->message,
-                            ['user_id' => $event->user->uuid, 'incident_id' => $event->incident->id]);
+                        $event->message,
+                        ['user_id' => $event->user->uuid, 'incident_id' => $event->incident->id]);
+                    Log::error("Device Token:" . $device->token . " send was attempted");
+
+                }else{
+                    Log::error("Device ID:" . $device->id . " token was null");
+                }
             }
         }
     }
