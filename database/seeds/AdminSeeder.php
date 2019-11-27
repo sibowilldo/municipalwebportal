54be7ccd39455f1cc87b0f0bac9318472b2d3b72
Sibongiseni Msomi <siboingiseni.msomi@outlook.com>
2019-11-27 20:13:57 +0200

    added zeh and oye onto seeder

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -13,7 +13,8 @@ class AdminSeeder extends Seeder
     {
         $faker = Faker::create();
         $statuses = \App\Status::pluck('id');
-        $user = \App\User::create([
+
+        $sah = \App\User::create([
             'uuid' => $faker->uuid,
             'firstname' => 'Sandile',
             'lastname' => 'Ndimande',
@@ -28,10 +29,7 @@ class AdminSeeder extends Seeder
             'created_at' => \Carbon\Carbon::now()->subDays(random_int(10,15)),
             'updated_at' => \Carbon\Carbon::now(),
             'deleted_at' => null]);
-
-        $user->roles()->attach([4]);
-
-        $user = \App\User::create([
+        $luh = \App\User::create([
             'uuid' => $faker->uuid,
             'firstname' => 'Lungisani',
             'lastname' => 'Blose',
@@ -46,10 +44,7 @@ class AdminSeeder extends Seeder
             'created_at' => \Carbon\Carbon::now()->subDays(random_int(10,15)),
             'updated_at' => \Carbon\Carbon::now(),
             'deleted_at' => null]);
-
-        $user->roles()->attach([4]);
-
-        $user = \App\User::create([
+        $sbo = \App\User::create([
             'uuid' => $faker->uuid,
             'firstname' => 'Sibongiseni',
             'lastname' => 'Msomi',
@@ -64,7 +59,41 @@ class AdminSeeder extends Seeder
             'created_at' => \Carbon\Carbon::now()->subDays(random_int(10,15)),
             'updated_at' => \Carbon\Carbon::now(),
             'deleted_at' => null]);
+        $oye = \App\User::create([
+            'uuid' => $faker->uuid,
+            'firstname' => 'Oye',
+            'lastname' => 'Oridota',
+            'contactnumber' => '0831234567',
+            'email' => 'oridotaoyebode@gmail.com',
+            'email_verified_at' => \Carbon\Carbon::now(),
+            'activation_token' => '',
+            'last_loggedin_at' => null,
+            'password' => bcrypt('admin123'), // secret
+            'remember_token' => str_random(10),
+            'status_id' => $faker->randomElement($array = $statuses),
+            'created_at' => \Carbon\Carbon::now()->subDays(random_int(10,15)),
+            'updated_at' => \Carbon\Carbon::now(),
+            'deleted_at' => null]);
+        $zeh = \App\User::create([
+            'uuid' => $faker->uuid,
+            'firstname' => 'Zethembiso',
+            'lastname' => 'Msomi',
+            'contactnumber' => '0831234567',
+            'email' => 'zethembiso.msomi@gmail.com',
+            'email_verified_at' => \Carbon\Carbon::now(),
+            'activation_token' => '',
+            'last_loggedin_at' => null,
+            'password' => bcrypt('admin123'), // secret
+            'remember_token' => str_random(10),
+            'status_id' => $faker->randomElement($array = $statuses),
+            'created_at' => \Carbon\Carbon::now()->subDays(random_int(10,15)),
+            'updated_at' => \Carbon\Carbon::now(),
+            'deleted_at' => null]);
 
-        $user->roles()->attach([4]);
+        $sah->roles()->attach([4]);
+        $luh->roles()->attach([4]);
+        $sbo->roles()->attach([4]);
+        $oye->roles()->attach([4]);
+        $zeh->roles()->attach([4]);
     }
 }
