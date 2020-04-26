395e29ddf8185d80c908726541bc52150d48062b
Sibongiseni Msomi <siboingiseni.msomi@outlook.com>
2020-04-26 05:34:03 +0200

    no message

--- resources/views/backend/departments/index.blade.php
+++ resources/views/backend/departments/index.blade.php
@@ -57,43 +57,6 @@
                             </li>
                         </ul>
                     </div>
-                    <div class="m-portlet__head-tools">
-                        <ul class="m-portlet__nav">
-                            <li class="m-portlet__nav-item">
-                                <div class="m-dropdown m-dropdown--inline m-dropdown--arrow m-dropdown--align-right m-dropdown--align-push" m-dropdown-toggle="hover" aria-expanded="true">
-                                    <a href="#" class="m-portlet__nav-link m-dropdown__toggle dropdown-toggle btn btn--sm m-btn--pill m-btn btn-outline-dark m-btn--hover-dark">
-                                        Quick Actions
-                                    </a>
-                                    <div class="m-dropdown__wrapper">
-                                        <span class="m-dropdown__arrow m-dropdown__arrow--right m-dropdown__arrow--adjust"></span>
-                                        <div class="m-dropdown__inner">
-                                            <div class="m-dropdown__body">
-                                                <div class="m-dropdown__content">
-                                                    <ul class="m-nav">
-                                                        <li class="m-nav__section m-nav__section--first">
-                                                            <span class="m-nav__section-text">Available Actions</span>
-                                                        </li>
-                                                        <li class="m-nav__item">
-                                                            <a href="{{ route('departments.create') }}" class="m-nav__link">
-                                                                <i class="m-nav__link-icon la la-building"></i>
-                                                                <span class="m-nav__link-text">Add New Department</span>
-                                                            </a>
-                                                        </li>
-                                                        <li class="m-nav__item">
-                                                            <a href="{{ route('districts.index') }}" class="m-nav__link">
-                                                                <i class="m-nav__link-icon la la-users"></i>
-                                                                <span class="m-nav__link-text">District</span>
-                                                            </a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
                 <div class="m-portlet__body">
 
